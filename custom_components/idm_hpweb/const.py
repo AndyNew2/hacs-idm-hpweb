65d51992f60d3eaab3e37bab87892b59a18005fb
Andy <145973686+AndyNew2@users.noreply.github.com>
2025-12-30 22:01:07 +0100

    Add CONF_STAT_DIV constant to const.py

--- custom_components/idm_hpweb/const.py
+++ custom_components/idm_hpweb/const.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 DOMAIN = "idm_hpweb"
 CONF_DISPLAY_NAME = "display_name"
 CONF_CYCLE_TIME = "CYCLE_TIME"
+CONF_STAT_DIV = "STATISTICS_DIV"
 # SIGNAL_IDM_TELEGRAM = "idm_telegram"
 DEF_DEVICE_NAME = "iDMwb"
 DEF_MIN_TIME_BETWEEN_UPDATES = timedelta(seconds=2)
